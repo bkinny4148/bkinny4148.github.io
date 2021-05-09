332053b8a36a8c07fee33604aa627cd2ca1891d9
bkinny4148 <benjamincmckinley@gmail.com>
2021-05-09 18:15:29 -0400

    Added wave border

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 A static personal website created using Jekyll and Github Pages to showcase some of my work as a student/developer.
 
 The site is constantly in development so if there is a feature that seems broken I am probably in the middle of working on a change.
+
+
+
+## TODO
+- parallax effect
+  - parallax on scroll?
+  - parallax relative to center of screen
+- bug with landing section expanding on mobile to fill screen
--- assets/js/geometric.js
+++ assets/js/geometric.js
@@ -1,3 +1,7 @@
+const DEBUG = false;
+
+
+
 /*
  * Make dollar sign symbol behave like jQuery.
  */
@@ -14,11 +18,18 @@ var $ = function(id) {
 var createShapes = function(number) {
 
   let html = '';
-  let parallax = 2;
 
-  for (var i = 1; i <= 50; i ++) {
-    html += '<div class="shape-container--'+i+'"><div data-parallax="5" class="random-shape"></div></div>';
-  }
+  //now decide custom parallax movement range for each shape to be stored in data-parallax attribute
+  let parallaxBase = 10; //max amount of parallax
+  let parallaxVariation = 5; //amount of variation in parallaxSpeed
+  let oppositeChance = .2; //probability shape will flow against normal flow
+
+  /* for each shape */
+  for (var i = 1; i <= number; i ++) {
+    let direction = Math.random() < oppositeChance ? -1 : 1;
+    let parallaxSpeed = (Math.random(parallaxVariation) + parallaxBase) * direction;
+    html += `<div class="shape-container--${i}"><div data-parallax="${parallaxSpeed}" class="random-shape"></div></div>`;
+  } //for
 
   document.querySelector('.shape').innerHTML += html;
 
@@ -35,9 +46,25 @@ var createShapes = function(number) {
  */
 function parallaxize(e, target, movement) {
 
-  var container = $('landing');
-  var relX = e.pageY - container.offset().left;
-  var relY = e.pageX - container.offset().top;
+  //run calculations to make the translation relative to difference from middle of screen
+  let width = screen.width;
+  let height = screen.height;
+
+  let mouseX = e.clientX;
+  let mouseY = e.clientY;
+
+  let diffX = mouseX - (width / 2);
+  let diffY = mouseY - (height / 2);
+
+  let shapes = document.querySelectorAll('.random-shape');
+  shapes.forEach((shape) => {
+
+    let movement = shape.dataset.parallax;
+    let relX = diffX / movement;
+    let relY = diffY / movement;
+
+    shape.style.transform = `translateX(${relX}px) translateY(${relY}px)`;
+  });
 
 } //parallaxize
 
@@ -46,22 +73,29 @@ function parallaxize(e, target, movement) {
  * Load functions/variables/listeners when window loads.
  */
 window.onload = function() {
-    createShapes();
+    createShapes(50); //number must match that within style sheet
 
     //add parallax listener to the landing area
     $('landing').addEventListener("mousemove",
       function(e) {
 
-        let movementRange = 5; //could be random for each shape and also randomly negative or positive
+        let mouseX = e.clientX;
+        let mouseY = e.clientY;
 
-        let mouseX = e.clientX / -5;
-        let mouseY = e.clientY / 5;
+        if (DEBUG) {
+          $('console').innerHTML = `mouseX:${mouseX}, mouseY:${mouseY}`
+        }
 
         let shapes = document.querySelectorAll('.random-shape');
         shapes.forEach((shape) => {
 
-          shape.style.transform = `translateX(${mouseX}px) translateY(${mouseY}px)`;
+          let movement = shape.dataset.parallax;
+          let relX = mouseX / movement;
+          let relY = mouseY / movement;
 
+          //shape.draggable();
+
+          shape.style.transform = `translateX(${relX}px) translateY(${relY}px)`;
         });
 
     });
